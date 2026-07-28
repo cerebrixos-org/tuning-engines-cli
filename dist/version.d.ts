321c5ae46ba4b674863aff6a1810ba5dbe783113
VC <bazooka720@gmail.com>
2026-07-28 16:51:22 -0400

    Release CLI and MCP 0.4.30

--- dist/version.d.ts
+++ dist/version.d.ts
@@ -1,3 +1,3 @@
-export declare const CLI_VERSION = "0.4.28";
-export declare const USER_AGENT = "tuning-engines-cli/0.4.28";
+export declare const CLI_VERSION = "0.4.30";
+export declare const USER_AGENT = "tuning-engines-cli/0.4.30";
 //# sourceMappingURL=version.d.ts.map
